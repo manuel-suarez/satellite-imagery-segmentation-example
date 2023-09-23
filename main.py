a4d3a7d29f69eb22431e53f56a66fc75b5669428
Arturo Suárez <manuel.suarez@cimat.mx>
2023-09-22 21:51:46 -0600

    Loading data

--- main.py
+++ main.py
@@ -6,3 +6,52 @@ import matplotlib.pyplot as plt
 from PIL import Image
 from patchify import patchify
 from sklearn.preprocessing import MinMaxScaler, StandardScaler
+
+# Initial setup
+dataset_root_folder = '/home/est_posgrado_manuel.suarez/data/'
+dataset_name = "DubaiDataset"
+minmaxscaler = MinMaxScaler();
+image_path_size = 256
+# Load data
+image_dataset = []
+mask_dataset = []
+
+for image_type in ['images', 'masks']:
+  if image_type == 'images':
+    image_extension = 'jpg'
+  elif image_type == 'masks':
+    image_extension = 'png'
+  for tile_id in range(1,8):
+    for image_id in range(1,20):
+      image = cv2.imread(os.path.join(dataset_root_folder, dataset_name, f"Tile {tile_id}", f"{image_type}", f"image_part_00{image_id}.{image_extension}"), 1)
+      if image is not None:
+        if image_type == 'masks':
+          image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        #print(image.shape)
+        size_x = (image.shape[1]//image_patch_size)*image_patch_size
+        size_y = (image.shape[0]//image_patch_size)*image_patch_size
+        #print("{} --- {} - {}".format(image.shape, size_x, size_y))
+        image = Image.fromarray(image)
+        image = image.crop((0,0,size_x,size_y))
+        #print("({},  {})".format(image.size[0],image.size[1]))
+        image = np.array(image)
+        patched_images = patchify(image, (image_patch_size, image_patch_size, 3), step=image_patch_size)
+        #print(len(patched_images))
+        for i in range(patched_images.shape[0]):
+          for j in range(patched_images.shape[1]):
+            if image_type == 'images':
+              individual_patched_image = patched_images[i,j,:,:]
+              #print(individual_patched_image.shape)
+              individual_patched_image = minmaxscaler.fit_transform(individual_patched_image.reshape(-1, individual_patched_image.shape[-1])).reshape(individual_patched_image.shape)
+              individual_patched_image = individual_patched_image[0]
+              #print(individual_patched_image.shape)
+              image_dataset.append(individual_patched_image)
+            elif image_type == 'masks':
+              individual_patched_mask = patched_images[i,j,:,:]
+              individual_patched_mask = individual_patched_mask[0]
+              mask_dataset.append(individual_patched_mask)
+
+image_dataset = np.array(image_dataset)
+mask_dataset = np.array(mask_dataset)
+print(len(image_dataset))
+print(len(mask_dataset))
