c4de92daffc0cc80751b255a1de4d9634a79e928
Arturo Suárez <manuel.suarez@cimat.mx>
2023-09-22 21:42:00 -0600

    Initial setup

---
+++ main.py
@@ -0,0 +1,8 @@
+import os
+import cv2
+import random
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image
+from patchify import patchify
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
