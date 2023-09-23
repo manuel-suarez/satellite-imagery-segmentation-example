2c6d128ac8a8b1dc1966a0f73036115bf3a9abb3
Arturo Suárez <manuel.suarez@cimat.mx>
2023-09-22 23:54:55 -0600

    Increase num of epochs to test metrics

--- main.py
+++ main.py
@@ -246,7 +246,7 @@ model.compile(optimizer="adam", loss=total_loss, metrics=metrics)
 model.summary()
 
 # Training
-model_history = model.fit(X_train, y_train, batch_size=16, verbose=1, epochs=100, validation_data=(X_test, y_test), shuffle=False)
+model_history = model.fit(X_train, y_train, batch_size=16, verbose=1, epochs=400, validation_data=(X_test, y_test), shuffle=False)
 # History
 history_a = model_history
 
